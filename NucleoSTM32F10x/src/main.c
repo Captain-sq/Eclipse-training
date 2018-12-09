feab7aa2a345b996b93f3e92634d8d637a47c075
Captain-sq <stringwringler@msn.com>
2018-12-09 09:55:43 +0100

    NucleoSTM32F103x:
    Replaced while(1) with for ever caluse for better understanding
    
    
    Signed-off-by: Captain-sq <stringwringler@msn.com>

--- NucleoSTM32F10x/src/main.c
+++ NucleoSTM32F10x/src/main.c
@@ -26,13 +26,14 @@
  */
 
 // ----------------------------------------------------------------------------
-
 #include <stdio.h>
 #include <stdlib.h>
 #include "diag/Trace.h"
 
 #include "Timer.h"
 #include "BlinkLed.h"
+
+#define ever    (;;)
 
 // ----------------------------------------------------------------------------
 //
@@ -77,9 +78,8 @@
 #pragma GCC diagnostic ignored "-Wmissing-declarations"
 #pragma GCC diagnostic ignored "-Wreturn-type"
 
-int
-main(int argc, char* argv[])
-{
+int main(int argc, char* argv[])
+  {
   // Send a greeting to the trace device (skipped on Release).
   trace_puts("Hello ARM World!");
 
@@ -90,25 +90,26 @@ main(int argc, char* argv[])
   timer_start();
 
   blink_led_init();
-  
+
   uint32_t seconds = 0;
 
   // Infinite loop
-  while (1)
+
+  for ever
     {
-      blink_led_on();
-      timer_sleep(seconds == 0 ? TIMER_FREQUENCY_HZ : BLINK_ON_TICKS);
+    blink_led_on();
+    timer_sleep(seconds == 0 ? TIMER_FREQUENCY_HZ : BLINK_ON_TICKS);
 
-      blink_led_off();
-      timer_sleep(BLINK_OFF_TICKS);
+    blink_led_off();
+    timer_sleep(BLINK_OFF_TICKS);
 
-      ++seconds;
+    ++seconds;
 
-      // Count seconds on the trace device.
-      trace_printf("Second %u\n", seconds);
+    // Count seconds on the trace device.
+    trace_printf("Second %u\n", seconds);
     }
   // Infinite loop, never return.
-}
+  }
 
 #pragma GCC diagnostic pop
 
